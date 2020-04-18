93aefb8900409f76ab0903563fe80cfc3b98efee
Antoine LZCH <antoine@agence-osez.com>
2020-04-18 12:49:01 +0200

    👌 IMPROVE: Change config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,13 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: 'AIzaSyAVL-LNlXRZYoZF9g2hDapN2lF2JN9ki4k',
+  authDomain: 'iimonitor.firebaseapp.com',
+  databaseURL: 'https://iimonitor.firebaseio.com',
+  projectId: 'iimonitor',
+  storageBucket: 'iimonitor.appspot.com',
+  messagingSenderId: '942441004379',
+  appId: '1:942441004379:web:3a5908f1dbba1a87deee7e'
 }
 
 firebase.initializeApp(config)
